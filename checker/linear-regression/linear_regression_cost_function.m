38cab4b10cf24a9c5f4d515df0105eb6982e713a
goptarov <ggoptarov@gmail.com>
2025-04-30 03:32:16 +0300

    Task 2.4

--- checker/linear-regression/linear_regression_cost_function.m
+++ checker/linear-regression/linear_regression_cost_function.m
@@ -1,8 +1,8 @@
 function [Error] = linear_regression_cost_function(Theta, Y, FeatureMatrix)
 
-  %deoarece putem ignora termenul theta 0, suma din cerinta va deveni
-  %doar norma la patrat a FeatureMatrix * Theta - Y(un vector al "punctelor" apartamentelor)
+  %deoarece putem ignora termenul theta 0, suma din cerinta va deveni doar
+  %norma la patrat a FeatureMatrix * Theta - Y(un vector al "punctelor" apartamentelor)
   m = size(FeatureMatrix, 1);
-  Theta(1) = [];
+  Theta(1) = []; %vectorul theta dat de cehcker are valoarea theta0 ca prim element, nu theta1 ca in cerinta.
   Error = (1/(2*m)) * (norm(FeatureMatrix * Theta - Y)) ^ 2;
 end
--- checker/linear-regression/parse_csv_file.m
+++ checker/linear-regression/parse_csv_file.m
@@ -1,8 +1,25 @@
 function [Y, InitialMatrix] = parse_csv_file(file_path)
-  % path -> a relative path to the .csv file
-  
-  % Y -> the vector with all actual values
-  % InitialMatrix -> the matrix that must be transformed
 
-  % TODO: parse_csv_file implementation
+    %chiar daca cerinta ne spune sa folosim csvread, acesta merge doar cu
+    %fisiere csv care contin numai numere. Noi avem si text in csv, asa ca vom
+    %citi fisierul normal.
+    fid = fopen(file_path, "r");
+    head = fgetl(fid); %scapam de prima linie din tabel
+
+    Y = {};
+    InitialMatrix = {};
+
+    line = fgetl(fid); %linia 1
+    row = 1; %index pentru linii
+    while ischar(line)
+      field = strsplit(line, ','); %impare linia
+
+      Y{row, 1} = field{1}; %primul element din field(linie) va merge in Y
+      InitialMatrix(row, :) = field(2:end); % restul merge in InitialMatrix
+
+      line = fgetl(fid);%trecem la urmatoarea linie
+      row = row + 1;
+    end
+
+    fclose(fid);
 end
